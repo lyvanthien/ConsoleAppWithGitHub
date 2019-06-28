d12566adb8e7cfc887edffe0dcf0377dbff9eba3
thienlv <thienlv1984@outlook.com.vn>
2019-06-28 17:38:52 +0700

    I have been added  Console.WriteLine("My name Ly Van Thien!!!"); into Program.cs

--- source/repos/ConsoleAppWithGitHub/ConsoleAppWithGitHub/Program.cs
+++ source/repos/ConsoleAppWithGitHub/ConsoleAppWithGitHub/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleAppWithGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World, I love you!!!");
+            Console.WriteLine("My name Ly Van Thien!!!");
         }
     }
 }
